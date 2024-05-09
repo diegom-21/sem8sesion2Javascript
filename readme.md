c1315e310b40ed72976df5385cb25efeeac4fd14
diegom-21 <jimmy.moreno@tecsup.edu.pe>
2024-05-08 22:30:02 -0500

    comit diego

---
+++ readme.md
@@ -0,0 +1,18 @@
+# JavaScript
+
+Una variable en JavaScript es un contenedor para almacenar datos. Puede contener datos de diferentes tipos, como números, strings, booleanos, objetos, arrays, etc.
+
+
+### Tipos de datos:
+
+1. **Números**: Se utiliza la variable "numero" para almacenar un número entero.
+   
+2. **String**: La variable "string" almacena un nombre como texto.
+   
+3. **Booleanos**: La variable "booleano" indica si una condición es verdadera o falsa.
+   
+4. **Arrays**: La variable "array" es un arreglo que contiene una serie de números.
+   
+5. **Objetos**: La variable "persona" es un objeto que contiene información sobre una persona, incluyendo nombre, edad y ciudad.
+
+
---
+++ script.js
@@ -0,0 +1,28 @@
+// 1. Números:
+var numero = 25;
+
+// 2. String:
+var string = "Juan";
+
+// 3. Booleanos:
+var booleano = true;
+
+// 4. Arreglos:
+var array = [1, 2, 3, 4, 5];
+
+// 5. Objetos:
+var persona = {
+  nombre: "María",
+  edad: 18,
+  ciudad: "Lima"
+};
+
+console.log("Que es una variable: Una variable es un contenedor que almacena un valor. Se utiliza para nombrar un dato que se puede usar en diferentes partes de un programa. Las variables se declaran utilizando la palabra clave var, let o const, seguida del nombre de la variable y un signo de igual (=). El valor de la variable se especifica después del signo de igual. Una variable en JavaScript es un contenedor para almacenar datos. Estos datos pueden ser de diferentes tipos, como números, strings, booleanos, objetos, arrays, etc.")
+console.log("----------------------------------------------------------------")
+
+console.log("Edad:", numero);
+console.log("Nombre:", string);
+console.log("¿Es mayor de edad?", booleano);
+console.log("Números:", array);
+console.log("Persona:", persona);
+
